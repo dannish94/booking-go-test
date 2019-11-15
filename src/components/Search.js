3d2517f7b66dc2cb66976a8c90321f9d0463a1bf
Dannish Hussain <dannish.hussain@intouchnetworks.com>
2019-11-15 11:15:15 +0000

    Initial search box created with styling applied

---
+++ src/components/Search.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+margin: 10px 0;
+`
+
+const SearchContainer = styled.div`
+display:flex;
+flex-direction: column;
+height:250px;
+background-color: #f3ce56;
+padding: 24px;
+min-width: 432px;
+align-items: flex-start;
+position: relative;
+`
+const SearchHeading = styled.h2`
+color: black;
+font-size: 30px;
+font-weight: 550;
+`
+
+const SearchLabel = styled.label`
+color: black;
+font-size: 16px;
+font-weight: 400;
+`
+
+const SearchInput = styled.input`
+width: 100%;
+min-width: 432px;
+height: 48px;
+font-size: 1rem;
+border: 1px solid #a4a4a4;
+border-radius: 4px;
+`
+
+class Search extends React.Component {
+
+    render()
+    {
+        return (
+            <SearchContainer className='search-container'>
+                <SearchHeading className='search-title'>Where are you going?</SearchHeading>
+                <SearchLabel className='search-label'>Pick-up Location</SearchLabel>
+                <StyledDiv className='form-group'>
+                    <SearchInput className='input' 
+                    placeholder='city, airport, station, region and district...'/>
+                </StyledDiv>
+            </SearchContainer>
+        )
+    }
+}
+
+export default Search
