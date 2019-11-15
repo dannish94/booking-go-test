c3e4d2c6f26a4d9f4f66b7a862be32dd8899726a
Dannish Hussain <dannish.hussain@intouchnetworks.com>
2019-11-15 12:37:26 +0000

    typo in import resolved

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../Appp';
+import App from '../App';
 
 it('renders App without crashing', () => {
   const div = document.createElement('div');
